8ef26df9ff225ddddfa44fb9fcb543f2e5fe4169
[firstname lastname] <132287423+martinsk91@users.noreply.github.com>
2024-09-09 17:06:09 +0200

    created function for register profile

---
+++ js/api/auth/register.js
@@ -0,0 +1,44 @@
+import * as variabels from "../variabels.js"
+const path = "/auth/register";
+const method = "POST";
+
+
+
+
+
+export async function register(profile) {
+    const registerURL = variabels.API_URL + path;
+    console.log(registerURL);
+    const body = JSON.stringify(profile);
+
+    try {
+        const response = await fetch(registerURL, {
+            headers: {
+                "content-type": "application/json"
+            },
+            method,
+            body
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error("Failed to register:", error);
+    }
+   
+}
+
+const profile = {
+    email: "example20111a11a1aa1a2a44.email@stud.noroff.no",
+    name: "HeiDu1112aa34a",
+    password: "heisann1234"
+}
+
+
+register(profile);
+
+
--- js/api/variabels.js
+++ js/api/variabels.js
@@ -1,2 +1,4 @@
 export const API_URL = 'https://v2.api.noroff.dev';
-export const listingsURL = '/auction/listings';
+export const auction = '/auction';
+const listing = '/listings'
+export const listingsURL = auction + listing;

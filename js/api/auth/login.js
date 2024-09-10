1c3addec2136ad47dcdde23892980cfb5f7c8443
[firstname lastname] <132287423+martinsk91@users.noreply.github.com>
2024-09-10 08:30:45 +0200

    created function for authorization login

---
+++ js/api/auth/login.js
@@ -0,0 +1,42 @@
+import * as variabels from "../variabels.js";
+import { save } from "../../storage/save.js";
+
+
+const path = "/auth/login";
+const method = "POST";
+
+export async function login(profile) {
+    const loginURL = variabels.API_URL + path;
+    const body = JSON.stringify(profile);
+
+    try {
+        const response = await fetch(loginURL, {
+            headers: {
+                "content-type": "application/json"
+            },
+            method,
+            body
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        const jsonResponse = await response.json();
+        const { accessToken, ...user } = jsonResponse.data;
+    
+       
+
+        save("token", accessToken);
+        save("profile", user);
+        
+    } catch (error) {
+        console.error("Login failed:", error);
+    }
+}
+
+
+
+
+
+

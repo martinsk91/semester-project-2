7df7c13db8bfe12907702fd0aec9bedab5696df4
[firstname lastname] <132287423+martinsk91@users.noreply.github.com>
2024-09-22 06:24:59 +0200

    updated rendering function to sort listings and have time-left

--- js/rendering/renderlistings.js
+++ js/rendering/renderlistings.js
@@ -1,65 +1,46 @@
-// import { listings } from "../listings/get.js";
-// export function renderListings(list){
-// //    console.log(listings.data[8]);
-//     const listingsContainer = 
-//     document.querySelector("#listingscontainer");
-//   if(listingsContainer){
-//     list.data.forEach(listing => {
-//     const listingDiv = document.createElement("div");
-//     const imageCropperDiv = document.createElement("div");
-//     const listingImg = document.createElement("img");
+
+
+import { newestFirst } from "../api/sort.js";
+import { formatTime } from "../listings/formattime.js";
+
+
         
        
-//         if (listing.media && listing.media.length > 0) {
-//             listingImg.src = listing.media[0].url;
-//             listingImg.alt = listing.title;
-//         }
-        
-       
-//         const title = document.createElement("h2");
-//         title.innerText = listing.title;
-//         const descriptionInfo = document.createElement("h3");
-//         descriptionInfo.innerText = "Description:";
-//         const description = document.createElement("p");
-//         description.innerText = listing.description;
-        
-//         imageCropperDiv.append(listingImg);
-//         listingDiv.append(imageCropperDiv, title, descriptionInfo, description);
-//         listingsContainer.append(listingDiv);
-     
 
-
-//         imageCropperDiv.classList.add("image-cropper");
-//         listingDiv.classList.add("col-12", "col-sm-6", "col-md-5", "col-lg-3", "bg-secondary", "shadow-lg", "rounded");
-//     listingImg.classList.add("w-100", "rounded", "p-3", "img-fluid");
-//     });
-
-// }
-// }
 
 export function renderListings(list){
+    newestFirst(list);
+    
     const listingsContainer = document.querySelector("#listingscontainer");
   
     if (listingsContainer) {
-        listingsContainer.innerHTML = "";  // Clear previous listings
+        listingsContainer.innerHTML = ""; 
 
         
-        // const listings = list.data ? list.data : list;
+        
     
     if(list.length > 0){
         list.forEach(listing => {
+            const formatedtime = formatTime(listing.endsAt);
+            console.log(listing.endsAt);
        
 
+            const link = document.createElement("a");
+            link.href = `/listing/index.html?id=${listing.id}`;
+            link.innerText = "see more";
+            link.classList.add("text-info")
             const listingDiv = document.createElement("div");
             const imageCropperDiv = document.createElement("div");
             const listingImg = document.createElement("img");
+            const date = document.createElement("p");
+            date.innerText = formatedtime;
 
 
             if (listing.media && listing.media.length > 0) {
                 listingImg.src = listing.media[0].url; 
                 listingImg.alt = listing.title;
-                // console.log(listing);
-                console.log(listing);
+                
+                
              } 
             
 
@@ -73,7 +54,9 @@ export function renderListings(list){
             description.innerText = listing.description;
 
             imageCropperDiv.append(listingImg);
-            listingDiv.append(imageCropperDiv, title, descriptionInfo, description);
+           
+            
+            listingDiv.append(imageCropperDiv, title, descriptionInfo, description, date, link);
             listingsContainer.append(listingDiv);
 
             imageCropperDiv.classList.add("image-cropper");

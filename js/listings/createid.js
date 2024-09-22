46fcd098a099f642ff774b66a10a97c30af2a396
[firstname lastname] <132287423+martinsk91@users.noreply.github.com>
2024-09-22 06:18:45 +0200

    updated function to get the bid amount for the specific listing

--- js/listings/createid.js
+++ js/listings/createid.js
@@ -1,43 +1,3 @@
-// import { API_URL } from "../api/variabels.js";
-
-// export async function fetchId(){
-//     const queryString = document.location.search;
-
-//     const params = new URLSearchParams(queryString);
-    
-//     const id = params.get("id");
-//     return id;
-
-     
-// }
-
-// const id = await fetchId();
-
-// const idURL = API_URL + "/" + id;
-
-// async function fetchListing() {
-//     try{
-//         const response = await fetch(idURL);
-
-//         if (!response.ok) {  
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         const result = await response.json();
-
-//         return result
-//     }
-//     catch(error){
-//         console.error("error fetching id", error);
-//         return null
-//     }
-
-    
-// }
-
-// export const specificListing = await fetchListing();
-
-
-
 import { API_URL, listing, auction } from "../api/variabels.js";
 
 
@@ -53,7 +13,7 @@ export function fetchId() {
 
 async function fetchListing() {
     const id = fetchId();
-    const idURL = API_URL + auction + listing + "/" + id;
+    const idURL = API_URL + auction + listing + "/" + id + "?_bids=true&_seller=true";
 
     try {
         const response = await fetch(idURL);
@@ -68,6 +28,7 @@ async function fetchListing() {
         console.error("error fetching id", error);
         return null;
     }
+    
 }
 
 
